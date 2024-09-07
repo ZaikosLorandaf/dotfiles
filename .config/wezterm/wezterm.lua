3150461991d9b2828f873d46ea7f6f8d07d55935
Sarah Gosselin <sarah@gosselin.xyz>
2024-09-07 18:46:39 -0400

    add wezterm and some font fixes

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("Vulf Mono")
+config.font_size = 16
+
+config.enable_tab_bar = false
+
+config.colors = {
+
+	-- background = "#111a1f",
+
+	brights = {
+		"#111a1f",
+		"#8d7856",
+		"#798362",
+		"#9b9257",
+		"#63768a",
+		"#b7416e",
+		"#6998b3",
+		"#c5c8c6",
+	},
+
+	ansi = {
+		-- Instead of the second red."#c0c7ca",
+		"#111a1f",
+		"#8d7856",
+		"#798362",
+		"#9b9257",
+		"#63768a",
+		"#b7416e",
+		"#6998b3",
+		"#c5c8c6",
+	},
+}
+
+config.window_background_opacity = 0.7
+
+return config
